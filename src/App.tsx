b3ddacde2ca638f0642f466563b06f873d64f117
Jia Ming <jma8774@bths.edu>
2022-03-01 20:16:18 -0500

    skeleton game, useArray/useGame hook, useArray test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, FC } from 'react';
+import { useGame } from './hooks/useGame'
 
-function App() {
-  return (
-    <div>
-      Hello World 2
+const App:FC = () => {
+  const [input, setInput] = useState<string>('')
+  const game = useGame()
+
+  const handleSubmit = (e: React.FormEvent): void => {
+    e.preventDefault()
+    game.guessWord(input)
+  }
+
+  return(
+    <div> 
+      {game.status}
+      <br/>
+      {game.guesses}
+      <br/>
+      {game.wordle} 
+      <br/>
+      <button onClick={game.newGame}> restart </button>
+      <br/>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input onChange={(e) => setInput(e.target.value)}></input>
+        <button> guess </button>
+      </form>
+      {game.history.data.map((word, index) => 
+        <div key={`${word}${index}`}> {word} </div>
+      )}
     </div>
-  );
+  ) 
 }
 
 export default App;
---
+++ src/hooks/useGame.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect, useRef } from 'react';
+import { useArray } from './useArray';
+
+// Returns random integer from [a, b]
+const randomInt = (start: number, end: number): number => {
+  return start + Math.floor(Math.random() * (end - start + 1))
+}
+
+export const useGame = () => {
+  const [guesses, setGuesses] = useState<number>(5)
+  const [wordle, setWordle] = useState<string>('')
+  const history = useArray<string>([])
+  const [status, setStatus] = useState<string>('ongoing')
+  const answers = useRef<string[]>([])
+  const words = useRef<Set<string>>(new Set())
+
+  useEffect(() => {
+    // Function to read my text file from the 'public' folder
+    const fetchWords = (filename: string): Promise<string[]> => {
+      return fetch(`${window.location.href}/${filename}`)
+      .then((res) => res.text())
+      .then((text) => {
+        return text.split('\n')
+      })
+    }
+    // Fetch words from answers.txt and pick a random one as the wordle after promise is resolved
+    fetchWords('answers.txt')
+    .then((text) => {
+      answers.current = text
+      newGame()
+    })
+    // Fetch words from words.txt
+    fetchWords('words.txt')
+    .then((text) => {
+      words.current = new Set(text.concat(answers.current))
+    })
+  }, [])
+  
+  const newGame = (): void => {
+    console.log("New game")
+    setGuesses(5)
+    const index = randomInt(0, answers.current.length)
+    setWordle(answers.current[index])
+  }
+
+  const guessWord = (word: string): void => {
+    if(!words.current.has(word) || status !== 'ongoing')
+      return
+    console.log(word === wordle ? "Correct guess" : "Wrong guess")
+    history.push(word)
+    setGuesses(guesses - 1)
+    if(word === wordle) {
+      setStatus('win')
+    }
+    else if(guesses-1 === 0) {
+      setStatus('lose')
+    }
+  }
+
+  return { guesses, wordle, history, status, newGame, guessWord } as const
+}
